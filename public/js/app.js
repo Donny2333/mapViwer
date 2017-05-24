de4f455e542e1a01d008ed9c52f3b79501a02734
Donny <1021001390@qq.com>
2017-05-24 21:33:49 +0800

    query id from front-end

--- public/js/app.js
+++ public/js/app.js
@@ -55,123 +55,146 @@ angular.module('mapViewer', [
 
 
         // 1. query map url by ID
-        var url = document.getElementById('vm.url').value;
-
-
-        // 2. get information of the map
-        var url1 = 'http://192.168.100.5:6080/arcgis/rest/services/IndustryMaps2/01_aqssfbt/MapServer';
-        var url2 = 'http://192.168.99.82:6080/arcgis/rest/services/MyMapService/MapServer';
-        var url3 = 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/';
-
-        // var url = url1;
-        var extent = [12426884.777, 3812409.2678, 12433705.749, 3815674.2327];
+        var url;
+        var Xmin;
+        var Ymin;
+        var Xmax;
+        var Ymax;
+        var srcID;
+        var extent = [];
         var pjson = {};
 
 
-        Map.load(url, {
-            f: 'pjson'
+        var id = _.last(_.split(window.location.pathname, '/'));
+        $http.post('http://172.30.1.246:9527/MapService.svc/GetMapDocList', {
+            docID: id,
+            pageNo: 0,
+            pageNum: 10
         }).then(function (res) {
-            if (res.status === 200 && res.data) {
-                pjson = res.data;
-                vm.list = pjson.layers;
-                initMap(url);
-            }
-        });
+            if (res.data.status === 'ok') {
+                console.log(res.data.result[0]);
+                url = res.data.result[0].MapServerPath;
+                Xmin = parseFloat(res.data.result[0].Xmin);
+                Ymin = parseFloat(res.data.result[0].Ymin);
+                Xmax = parseFloat(res.data.result[0].Xmax);
+                Ymax = parseFloat(res.data.result[0].Ymax);
+                extent = [Xmin, Ymin, Xmax, Ymax];
+                srcID = res.data.result[0].SrcID;
+                Map.load(url, {
+                    f: 'pjson'
+                }).then(function (res) {
+                    if (res.status === 200 && res.data) {
+                        pjson = res.data;
+                        vm.list = pjson.layers;
+                        initMap(url);
+                    }
+                });
 
 
-        // 3. create map
-        var container = document.getElementById('popup');
-        var content = document.getElementById('popup-content');
-        var closer = document.getElementById('popup-closer');
-
-        var overlay = new ol.Overlay({
-            element: container,
-            autoPan: true,
-            autoPanAnimation: {
-                duration: 250
-            }
-        });
-
-        $compile(container)($scope);
-
-        $scope.swipe = function (delta) {
-            var contents = vm.contents.data;
-            var id = (vm.contents.id += delta);
-            vm.popup.layerName = contents[id].layerName;
-            vm.popup.attribute = contents[id].attributes;
-            return false;
-        };
-
-        $scope.close = function () {
-            overlay.setPosition(undefined);
-            closer.blur();
-            return false;
-        };
-
-        var map = new ol.Map({
-            layers: [new ol.layer.Image()],
-            overlays: [overlay],
-            target: 'map',
-            view: new ol.View({
-                center: [(extent[0] + extent[2]) / 2, (extent[1] + extent[3]) / 2],
-                // zoom: 15,
-                extent: extent,
-                resolution: 96
-            })
-        });
+                // 2. get information of the map
+                var url1 = 'http://192.168.100.5:6080/arcgis/rest/services/IndustryMaps2/01_aqssfbt/MapServer';
+                var url2 = 'http://192.168.99.82:6080/arcgis/rest/services/MyMapService/MapServer';
+                var url3 = 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/';
 
 
-        // 4. add popup event and controls
-        map.on('singleclick', function (evt) {
-            var coordinate = evt.coordinate;
-            var x = coordinate[0];
-            var y = coordinate[1];
+                // 3. create map
+                var container = document.getElementById('popup');
+                var content = document.getElementById('popup-content');
+                var closer = document.getElementById('popup-closer');
 
-            Map.identify(url, {
-                geometry: [x - .00005, y - .00005, x + .00005, y + .00005].join(','),
-                geometryType: "esriGeometryEnvelope",
-                layers: 'all',
-                tolerance: 10,
-                sr: '3857',
-                mapExtent: map.getView().calculateExtent(),
-                imageDisplay: _.union(map.getSize(), [map.getView().getResolution()]),
-                f: "json"
-            }).then(function (res) {
-                if (res.status === 200 && res.data && res.data.results && res.data.results.length) {
-                    var contents = vm.contents.data = res.data.results;
-                    var id = vm.contents.id = 0;
+                var overlay = new ol.Overlay({
+                    element: container,
+                    autoPan: true,
+                    autoPanAnimation: {
+                        duration: 250
+                    }
+                });
+                console.log(extent);
+
+                var map = new ol.Map({
+                    layers: [new ol.layer.Image()],
+                    overlays: [overlay],
+                    target: 'map',
+                    view: new ol.View({
+                        center: [(extent[0] + extent[2]) / 2, (extent[1] + extent[3]) / 2],
+                        // zoom: 15,
+                        extent: extent,
+                        projection: 'EPSG:' + srcID,
+                        resolution: 96
+                    })
+                });
+
+                $compile(container)($scope);
+
+                $scope.swipe = function (delta) {
+                    var contents = vm.contents.data;
+                    var id = (vm.contents.id += delta);
                     vm.popup.layerName = contents[id].layerName;
                     vm.popup.attribute = contents[id].attributes;
+                    return false;
+                };
 
-                    var html = "<div ng-include=\"'popup-content.html'\"></div>";
-                    content.innerHTML = '';
-                    $compile(html)($scope).appendTo(content);
-                    overlay.setPosition(coordinate);
+                $scope.close = function () {
+                    overlay.setPosition(undefined);
+                    closer.blur();
+                    return false;
+                };
+
+
+                // 4. add popup event and controls
+                map.on('singleclick', function (evt) {
+                    var coordinate = evt.coordinate;
+                    var x = coordinate[0];
+                    var y = coordinate[1];
+
+                    Map.identify(url, {
+                        geometry: [x - .00005, y - .00005, x + .00005, y + .00005].join(','),
+                        geometryType: "esriGeometryEnvelope",
+                        layers: 'all',
+                        tolerance: 10,
+                        sr: '3857',
+                        mapExtent: map.getView().calculateExtent(),
+                        imageDisplay: _.union(map.getSize(), [map.getView().getResolution()]),
+                        f: "json"
+                    }).then(function (res) {
+                        if (res.status === 200 && res.data && res.data.results && res.data.results.length) {
+                            var contents = vm.contents.data = res.data.results;
+                            var id = vm.contents.id = 0;
+                            vm.popup.layerName = contents[id].layerName;
+                            vm.popup.attribute = contents[id].attributes;
+
+                            var html = "<div ng-include=\"'popup-content.html'\"></div>";
+                            content.innerHTML = '';
+                            $compile(html)($scope).appendTo(content);
+                            overlay.setPosition(coordinate);
+                        }
+                    });
+                });
+
+                function initMap(url, params) {
+                    map.getLayers().item(0).setSource(new ol.source.ImageArcGISRest({
+                        url: url,
+                        params: params || {}
+                    }));
                 }
-            });
-        });
 
-        function initMap(url, params) {
-            map.getLayers().item(0).setSource(new ol.source.ImageArcGISRest({
-                url: url,
-                params: params || {}
-            }));
-        }
+                // remove Attribution control
+                map.getControls().forEach(function (control) {
+                    if (control instanceof ol.control.Attribution) {
+                        map.getControls().remove(control);
+                    }
+                });
 
-        // remove Attribution control
-        map.getControls().forEach(function (control) {
-            if (control instanceof ol.control.Attribution) {
-                map.getControls().remove(control);
+                // add zoomSlider control
+                var zoomslider = new ol.control.ZoomSlider();
+                map.addControl(zoomslider);
+
+                // add scaleLine control
+                var scaleLine = new ol.control.ScaleLine({
+                    units: 'metric'
+                });
+                map.addControl(scaleLine);
+
             }
         });
-
-        // add zoomSlider control
-        var zoomslider = new ol.control.ZoomSlider();
-        map.addControl(zoomslider);
-
-        // add scaleLine control
-        var scaleLine = new ol.control.ScaleLine({
-            units: 'metric'
-        });
-        map.addControl(scaleLine);
     }]);
--- routes/map.js
+++ routes/map.js
@@ -8,22 +8,7 @@ var router = express.Router();
 
 /* GET map id. */
 router.get('/:id', function (req, res, next) {
-    var id = req.params.id;
-    var url = '';
-    request.post({
-        url: 'http://192.168.99.105:9527/MapService.svc/GetMapDocList',
-        body: JSON.stringify({
-            docID: id,
-            pageNo: 0,
-            pageNum: 10
-        })
-    }, function (request, response) {
-        var data = JSON.parse(response.body);
-        if (data.result && data.result.length) {
-            url = data.result[0].MapServerPath;
-        }
-        res.render('map', {URL: url});
-    });
+    res.render('map');
 });
 
 module.exports = router;
